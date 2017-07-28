173351358dc1dd29fe2b3d65b0bb9c27a9dc11d6
Neo <neo.li@jam3.com>
2017-07-28 12:07:16 -0400

    :rocket:

---
+++ Assets/Neo/WRLDARKitAnchorHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class WRLDARKitAnchorHandler : MonoBehaviour 
+{
+	private UnityARAnchorManager m_unityARAnchorManager;
+	[SerializeField]
+	private Transform m_wrldMapParent;
+
+	[SerializeField]
+	private Transform m_wrldMapMask;
+
+	void Start()
+	{
+		//Create an instance of UnityARAnchorManager so we can get details for anchors
+		m_unityARAnchorManager = new UnityARAnchorManager();
+	}
+
+	void Update()
+	{
+		//Getting a list of all anchors
+		List<ARPlaneAnchorGameObject> arpags = m_unityARAnchorManager.GetCurrentPlaneAnchors ();
+
+		//For the sake of this tutorial we will simply position our map on the first anchor that we find
+		if (arpags.Count >= 1) 
+		{
+			ARPlaneAnchor arPlaneAnchor = arpags [0].planeAnchor;
+
+			//Setting the position of our map to match the position of anchor
+			m_wrldMapParent.position = UnityARMatrixOps.GetPosition (arPlaneAnchor.transform);
+
+			//Updating our mask according to the ARKit anchor
+			m_wrldMapMask.rotation = UnityARMatrixOps.GetRotation (arPlaneAnchor.transform);
+			m_wrldMapMask.localPosition = new Vector3(arPlaneAnchor.center.x, arPlaneAnchor.center.y, -arPlaneAnchor.center.z);
+			m_wrldMapMask.localScale  = new Vector3(arPlaneAnchor.extent.x, m_wrldMapMask.localScale.y, arPlaneAnchor.extent.z);
+		}
+	}
+}
---
+++ Assets/Neo/WRLDARStreamingCameraHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WRLDARStreamingCameraHandler : MonoBehaviour 
+{
+	[SerializeField]
+	private Transform m_wrldMapTransform;
+
+	//We want to make sure that our Streaming Camera streams that same area of the map
+	//that is being observer by the ARKit Camera. To achieve this we will set the position and
+	//rotation of our Streaming Camera according to the ARKit Camera.
+	void Update () 
+	{
+		//Getting the main camera. In our case this will be the ARKit Camera
+		Transform mainCameraTransform = Camera.main.transform;
+
+		//We want our streaming camera to look where ever we are looking
+		transform.rotation = mainCameraTransform.rotation;
+		//Transforming the ARKit camera position so our Streaming Camera
+		//positions itself according to our WRLD Scale
+		transform.position = m_wrldMapTransform.InverseTransformPoint (mainCameraTransform.position);
+	}
+}
